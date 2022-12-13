31a43c4494c0dabd6418295b77147997399c3635
Akash Rahman <mraakash01932@gmail.com>
2022-12-13 19:48:16 +0600

    navbar customize

--- components/Navigation.js
+++ components/Navigation.js
@@ -51,55 +51,54 @@ const Navigation = () => {
     });
   }, []);
   return (
-    <div
-      style={{ top: visible ? "0" : "-113px" }}
-      className="w-full transition-all shadow-xl mb-10 px-3 sm:px-0 fixed bg-white z-40"
-    >
-      <div className="container mx-auto h-28 flex justify-between items-center">
-        <div className="w-1/3 hidden md:block">
-          {/* <div className="flex gap-3 items-center mb-3">
+    <>
+      <div
+        style={{ top: visible ? "0" : "-113px" }}
+        className="w-full transition-all shadow-xl mb-10 px-3 sm:px-0 fixed bg-white z-40"
+      >
+        <div className="container mx-auto h-28 flex justify-between items-center">
+          <div className="w-1/3 hidden md:block">
+            {/* <div className="flex gap-3 items-center mb-3">
             <input className="input input-xs bg-transparent input-secondary rounded-none focus:outline-none" />
             <FaSearch className="text-xl cursor-pointer" />
           </div>
           <h5 className="text-xs">মঙ্গলবার, ০৬ ডিসেম্বর ২০২২</h5> */}
-        </div>
-        <div className="w-1/3 block md:hidden">
-          <div className="block">
-            <FaBars
-              onClick={() => setHidden(!hidden)}
-              className="text-xl cursor-pointer"
-            />
           </div>
-        </div>
-        <div className="w-1/3 flex justify-center items-center h-full">
-          <Link href="/">
-            <img className="w-48" src={logo} alt="" />
-          </Link>
-        </div>
-        <div className="w-1/3 text-right">
-          {/* <Link href="/user/login">
+          <div className="w-1/3 block md:hidden">
+            <div className="block">
+              <FaBars
+                onClick={() => setHidden(!hidden)}
+                className="text-xl cursor-pointer"
+              />
+            </div>
+          </div>
+          <div className="w-1/3 flex justify-center items-center h-full">
+            <Link href="/">
+              <img className="w-48" src={logo} alt="" />
+            </Link>
+          </div>
+          <div className="w-1/3 text-right">
+            {/* <Link href="/user/login">
             <button className="btn btn-sm rounded-none border bg-transparent text-blue-500">
               Login
             </button>
           </Link> */}
+          </div>
         </div>
-      </div>
-      <div
-        className={`${
-          hidden ? "hidden" : "block"
-        } md:block py-3 border-t-2 flex flex-wrap gap-5 justify-center min-h-[50px] transition-all`}
-      >
-        <ul className="px-5 grid grid-cols-2 sm:grid-cols-3 md:flex md:flex-wrap md:justify-center gap-5">
-          {nav.map((item) => {
-            return (
-              <li key={item.key}>
-                <Link href={`/category/${item?.name}`} legacyBehavior>
-                  <a className="hover:text-blue-500">{item?.name}</a>
-                </Link>
-              </li>
-            );
-          })}
-          {/* <li>
+        <div
+          className={`hidden md:relative md:bg-transparent py-3 border-t-2 md:flex flex-wrap gap-5 justify-center min-h-[50px] transition-all`}
+        >
+          <ul className="px-5 grid grid-cols-2 sm:grid-cols-3 md:flex md:flex-wrap md:justify-center gap-5">
+            {nav.map((item) => {
+              return (
+                <li key={item.key}>
+                  <Link href={`/category/${item?.name}`} legacyBehavior>
+                    <a className="hover:text-blue-500">{item?.name}</a>
+                  </Link>
+                </li>
+              );
+            })}
+            {/* <li>
             <Link href="/category/aksh" legacyBehavior>
               <a className="hover:text-blue-500">Life-style</a>
             </Link>
@@ -124,9 +123,39 @@ const Navigation = () => {
               <a className="hover:text-blue-500">Sports</a>
             </Link>
           </li> */}
-        </ul>
+          </ul>
+        </div>
       </div>
-    </div>
+      <div className="block md:hidden">
+        <div
+          className={`${
+            hidden
+              ? "hidden"
+              : "block  fixed bg-white top-0 left-0 overflow-scroll min-w-[250px] h-screen z-50"
+          } py-3 border-t-2 grid gap-5 justify-start  transition-all right-shadow`}
+        >
+          <div className="block pl-5 py-3">
+            <FaBars
+              onClick={() => setHidden(!hidden)}
+              className="text-xl cursor-pointer"
+            />
+          </div>
+          <div>
+            <ul className="px-5 grid grid-cols-1 sm:grid-cols-1 md:flex md:flex-wrap md:justify-center gap-5">
+              {nav.map((item) => {
+                return (
+                  <li className="" key={item.key}>
+                    <Link href={`/category/${item?.name}`} legacyBehavior>
+                      <a className="hover:text-blue-500">{item?.name}</a>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
