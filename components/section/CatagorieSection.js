49a0d348f13379932903dd9eb4e108ea88e71357
Akash Rahman <mraakash01932@gmail.com>
2022-12-21 01:48:43 +0600

    category section  modified

--- components/section/CatagorieSection.js
+++ components/section/CatagorieSection.js
@@ -1,4 +1,5 @@
 import React from "react";
+import RactangleCard from "../RactangleCard";
 import SectionHeader from "../SectionHeader";
 import SquareCard from "../SquareCard";
 import TextCard from "../TextCard";
@@ -6,18 +7,18 @@ import TextCard from "../TextCard";
 const CatagorieSection = ({ news }) => {
   return (
     <>
-      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-5">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
         {news &&
           news?.map((item) => {
             return (
-              <div className="custom-border">
+              <div className="">
                 <SectionHeader
                   title={item?.name}
                   link={`/category/${item?.name}`}
                 />
                 <SquareCard news={item?.news[0]} />
-                <TextCard news={item?.news[1]} />
-                <TextCard news={item?.news[2]} />
+                <RactangleCard news={item?.news[1]} />
+                <RactangleCard news={item?.news[2]} />
               </div>
             );
           })}
