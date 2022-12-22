a93e83cef25611068987e2a624c82abd02dec9c8
Akash Rahman <mraakash01932@gmail.com>
2022-12-22 23:52:11 +0600

    container modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: "576px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "992px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1200px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1400px",
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {},
   },
   plugins: [
@@ -14,19 +30,19 @@ module.exports = {
         ".container": {
           maxWidth: "100%",
           "@screen sm": {
-            maxWidth: "620px",
+            maxWidth: "540px",
           },
           "@screen md": {
-            maxWidth: "728px",
+            maxWidth: "720px",
           },
           "@screen lg": {
-            maxWidth: "1000px",
+            maxWidth: "960px",
           },
           "@screen xl": {
-            maxWidth: "1240px",
+            maxWidth: "1140px",
           },
           "@screen 2xl": {
-            maxWidth: "1500px",
+            maxWidth: "1320px",
           },
         },
       });
